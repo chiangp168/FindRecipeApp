25fd19095a0a24d365fca774d561f3dd791823c3
Nina Le <le.ngan@husky.neu.edu>
2020-11-04 22:24:11 -0800

    added nutrition model

--- src/recipe/model/NutritionFacts.java
+++ src/recipe/model/NutritionFacts.java
@@ -2,4 +2,88 @@ package recipe.model;
 
 public class NutritionFacts {
 
+	protected double calories;
+	protected int totalFat;
+	protected int sugar;
+	protected int sodium;
+	protected int protein;
+	protected int saturatedFat;
+	protected int carb;
+	protected int recipeId;
+
+	public NutritionFacts(double calories, int totalFat, int sodium, int protein, int saturatedFat, int carb, int recipeId) {
+		this.calories = calories;
+		this.totalFat = totalFat;
+		this.sodium = sodium;
+		this.protein = protein;
+		this.saturatedFat = saturatedFat;
+		this.carb = carb;
+		this.recipeId = recipeId;
+	}
+	
+	public double getCalories() {
+		return calories;
+	}
+
+	public void setCalories(double calories) {
+		this.calories = calories;
+	}
+
+	public int getTotalFat() {
+		return totalFat;
+	}
+
+	public void setTotalFat(int totalFat) {
+		this.totalFat = totalFat;
+	}
+
+	public int getSugar() {
+		return sugar;
+	}
+
+	public void setSugar(int sugar) {
+		this.sugar = sugar;
+	}
+
+	public int getSodium() {
+		return sodium;
+	}
+
+	public void setSodium(int sodium) {
+		this.sodium = sodium;
+	}
+
+	public int getProtein() {
+		return protein;
+	}
+
+	public void setProtein(int protein) {
+		this.protein = protein;
+	}
+
+	public int getSaturatedFat() {
+		return saturatedFat;
+	}
+
+	public void setSaturatedFat(int saturatedFat) {
+		this.saturatedFat = saturatedFat;
+	}
+
+	public int getCarb() {
+		return carb;
+	}
+
+	public void setCarb(int carb) {
+		this.carb = carb;
+	}
+
+	public int getRecipeId() {
+		return recipeId;
+	}
+
+	public void setRecipeId(int recipeId) {
+		this.recipeId = recipeId;
+	}
+	
 }
+
