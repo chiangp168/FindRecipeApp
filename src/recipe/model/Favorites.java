cb5592b20c489add664f971d9c4da36f7a391af5
xiangyi0818 <34527543+xiangyi0818@users.noreply.github.com>
2020-11-08 20:41:08 -0800

    add rating and favorite model

--- src/recipe/model/Favorites.java
+++ src/recipe/model/Favorites.java
@@ -1,5 +1,41 @@
 package recipe.model;
 
+import recipe.model.Users;
+import recipe.model.Ratings;
+
 public class Favorites {
+	protected int favoriteId;
+	protected Users user;
+	protected Recipes recipe;
+	
+	public Favorites(int favoriteId,Users user,Recipes recipe) {
+		this.favoriteId = favoriteId;
+		this.user = user;
+		this.recipe = recipe;
+	}
+	
+	public int getFavoriteId() {
+		return favoriteId;
+	}
+
+	public void setFavoriteId(int favoriteId) {
+		this.favoriteId = favoriteId;
+	}
+	
+	public Users getUser() {
+		return user;
+	}
+
+	public void setUser(Users user) {
+		this.user = user;
+	}
+	
+	public Recipes getRecipe() {
+		return recipe;
+	}
+
+	public void setRecipe(Recipes recipe) {
+		this.recipe = recipe;
+	}	
 
 }
